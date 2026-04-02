a501843a753d5621926ed34b3f6806eec8011741
mrCherry97 <mwisnia97@gmail.com>
2026-04-02 12:55:43 +0200

    fix 404

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,15 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
+if (window.location.search.startsWith("?/")) {
+  const decodedPath = window.location.search
+    .slice(1)
+    .split("&")
+    .map((segment) => segment.replace(/~and~/g, "&"))
+    .join("?");
+
+  const normalizedPath = `${window.location.pathname.replace(/\/$/, "")}${decodedPath}${window.location.hash}`;
+  window.history.replaceState(null, "", normalizedPath);
+}
+
 createRoot(document.getElementById("root")!).render(<App />);
